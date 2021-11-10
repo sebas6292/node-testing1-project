4282ef7b337ae4db8a36b3b13ffc88f348cb16f7
Sebastian <sebas6292@gmail.com>
2021-11-10 14:44:42 -0800

    wip

--- index.js
+++ index.js
@@ -85,6 +85,8 @@ class Seasons {
    */
   constructor() {
     // ✨ initialize whatever properties are needed
+    this.seasons = ['summer', 'fall', 'winter', 'spring']
+    this.currentSeason = 0
   }
 
   /**
@@ -101,6 +103,13 @@ class Seasons {
    */
   next() {
     // ✨ implement
+    const result = this.seasons[this.currentSeason]
+    if (this.currentSeason === 3) {
+      this.currentSeason = 0
+    } else {
+      ++this.currentSeason
+    }
+      return result
   }
 }
 
