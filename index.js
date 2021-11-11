dcf28c9d7cf33b91b234b7fdcbb1abf66705c4b0
Sebastian <sebas6292@gmail.com>
2021-11-10 17:44:44 -0800

    wip

--- index.js
+++ index.js
@@ -123,7 +123,8 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -141,6 +142,15 @@ class Car {
    */
   drive(distance) {
     // ✨ implement
+    const milesCanDrive = this.tank * this.mpg
+    if (distance <= milesCanDrive) {
+      this.odometer = this.odometer + distance
+      this.tank = this.tank - (distance / this.mpg)
+      return this.odometer
+    } 
+    this.odometer = this.odometer + milesCanDrive
+    this.tank = 0
+    return this.odometer
   }
 
   /**
@@ -156,6 +166,14 @@ class Car {
    */
   refuel(gallons) {
     // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank
+    if (gallons <= gallonsThatFit) {
+      this.tank = this.tank + gallons
+    } else { 
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
+  
   }
 }
 
@@ -174,6 +192,7 @@ class Car {
  */
 function isEvenNumberAsync(number) {
   // ✨ implement
+  return number % 2 === 0 || false
 }
 
 module.exports = {
